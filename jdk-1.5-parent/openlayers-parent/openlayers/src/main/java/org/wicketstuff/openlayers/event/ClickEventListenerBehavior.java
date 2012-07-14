26d9776d7b5808a163736b6a95a9ec187ae7ba3d
Erwin Bolwidt <ebol.github@worldturner.com>
2012-07-14 16:54:44 +0800

    Allow Click event listeners to receive the zoom level at the moment of the click

--- jdk-1.5-parent/openlayers-parent/openlayers/src/main/java/org/wicketstuff/openlayers/event/ClickEventListenerBehavior.java
+++ jdk-1.5-parent/openlayers-parent/openlayers/src/main/java/org/wicketstuff/openlayers/event/ClickEventListenerBehavior.java
@@ -5,34 +5,45 @@ import org.apache.wicket.request.Request;
 import org.apache.wicket.request.cycle.RequestCycle;
 import org.wicketstuff.openlayers.api.LonLat;
 
-public abstract class ClickEventListenerBehavior extends EventListenerBehavior
-{
+public abstract class ClickEventListenerBehavior extends EventListenerBehavior {
 
 	private static final long serialVersionUID = 1L;
 
+	// TODO: whenever any type of event arrives, update the map object (the component of this behavior),
+	// with the default event information, which has the map center, bounds and zoom, normal and converted.
+	
 	@Override
-	public String getJSaddListener()
-	{
+	public String getJSaddListener() {
 		return getOpenLayersMap().getJSinvoke("addClickListener('" + getCallbackUrl() + "')");
 	}
 
 	@Override
-	protected String getEvent()
-	{
+	protected String getEvent() {
 		return "click";
 	}
 
 	@Override
-	protected void onEvent(AjaxRequestTarget target)
-	{
+	protected void onEvent(AjaxRequestTarget target) {
 		Request request = RequestCycle.get().getRequest();
-		String lon = request.getRequestParameters().getParameterValue("lonConverted").toString();
-		String lat = request.getRequestParameters().getParameterValue("latConverted").toString();
-		LonLat lonLat = new LonLat(Double.parseDouble(lon), Double.parseDouble(lat));
+		double lon = request.getRequestParameters().getParameterValue("lonConverted").toDouble();
+		double lat = request.getRequestParameters().getParameterValue("latConverted").toDouble();
+		int zoom = request.getRequestParameters().getParameterValue("zoom").toInt();
+		LonLat lonLat = new LonLat(lon, lat);
+		onClick(lonLat, zoom, target);
 		onClick(lonLat, target);
 
 	}
 
-	protected abstract void onClick(LonLat lonLat, AjaxRequestTarget target);
+	/**
+	 * To be implemented when the zoom level is not of interest.
+	 */
+	protected void onClick(LonLat lonLat, AjaxRequestTarget target) {
+	}
+
+	/**
+	 * To be implemented when the zoom level is of interest.
+	 */
+	protected void onClick(LonLat lonLat, int zoom, AjaxRequestTarget target) {
+	}
 
 }
